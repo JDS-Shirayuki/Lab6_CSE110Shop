71ac1b122748d7dea00067231cbbaed2a48692c1
JDS-Shirayuki <44740078+JDS-Shirayuki@users.noreply.github.com>
2021-02-13 22:21:18 -0800

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -2,4 +2,16 @@
 
 window.addEventListener('DOMContentLoaded', () => {
   // TODO
+  fetch('https://fakestoreapi.com/products')
+  .then(response => response.json())
+  .then(data => {
+    localStorage.setItem('products', JSON.stringify(data));
+  });
+
+  if (localStorage.getItem('products') != null) {
+    var products = JSON.parse(localStorage.getItem('products'));
+    for (var i = 0; i < products.length; ++i) {
+      document.getElementById('product-list').appendChild(new ProductItem(products[i]));
+    }
+  }
 });
